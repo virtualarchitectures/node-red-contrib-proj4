0b8ac4b20caac5bb291a25f9c59ce09e9d627f6a
virtualarchitectures <oliver.dawkins.13@ucl.ac.uk>
2019-11-24 22:53:43 +0000

    Function updated to process Proj  and WKT strings

--- proj4/proj4.js
+++ proj4/proj4.js
@@ -23,16 +23,21 @@ module.exports = function(RED) {
 		        'EPSG:3857', 'PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],AUTHORITY["EPSG","3857"]]'
 		        ]
 	    	]);
+			
+			node.select = n.select;
+
 			node.firstProjection = n.firstProjection;
       		node.secondProjection = n.secondProjection;
+
+      		node.firstCRS = n.firstCRS;
+      		node.secondCRS = n.secondCRS;
       
       		node.input_coords;
       		node.output_coords;
       		var err;
-      		
-      		//console.log('Converting from ' + node.firstProjection + ' to ' + node.secondProjection);
 
-	  		if (msg.payload.x && msg.payload.y) {
+      		//console.log('Reading Incomming Payload');
+  			if (msg.payload.x && msg.payload.y) {
 	    		node.input_coords = {"x":msg.payload.x,"y":msg.payload.y};
 	    	} else if (msg.payload.lon && msg.payload.lat) {
 	    		node.input_coords = {"x":msg.payload.lon,"y":msg.payload.lat};
@@ -43,9 +48,20 @@ module.exports = function(RED) {
 	  		} else {
 	  			node.input_coords = msg.payload;
 	  		}
-		  
-	  		node.output_coords = proj4(proj4.defs[node.firstProjection],proj4.defs[node.secondProjection],node.input_coords);
-	  		msg.proj4_coords = node.output_coords;
+
+      		if (node.select === "epsg_code") {
+
+	      		//console.log('Converting from ' + node.firstProjection + ' to ' + node.secondProjection)
+		  		node.output_coords = proj4(proj4.defs[node.firstProjection],proj4.defs[node.secondProjection],node.input_coords);
+		  		msg.proj4_coords = node.output_coords;
+
+		  	} else if (node.select === "crs_string") {
+		  		
+		  		//console.log('Converting using Proj / WKT Strings');
+		  		node.output_coords = proj4(node.firstCRS,node.secondCRS,node.input_coords);
+		  		msg.proj4_coords = node.output_coords;
+
+		  	}
 
 			if(err) {
 				if (done) {
